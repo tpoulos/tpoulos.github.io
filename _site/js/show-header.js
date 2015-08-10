542b4d07331010b394dfcfef495ae684da2f96e0
Taylor Poulos <poulos.taylor.w@gmail.com>
2015-08-09 21:57:56 -0400

    use typekit for fonts

--- _site/js/show-header.js
+++ _site/js/show-header.js
@@ -1,27 +1,34 @@
 $(document).ready(function() {
   var $articleTitle = $(document).find(".article-title:first");
-  var headerIsShown = isBelowTitle();
+  var headerIsShown = true;
 
   if($(".article").length) {
 
     var titleDisplacement = $articleTitle.outerHeight();
 
-    $(document).scroll(function(e) {
-      if(headerIsShown && !isBelowTitle()) {
-        $("header").removeClass("is-shown").addClass("is-hidden");
+    updateHeader();
 
-        headerIsShown = false;
-      }
-      if(!headerIsShown && isBelowTitle()) {
-        $("header").removeClass("is-hidden").addClass("is-shown");
-        headerIsShown = true;
-      }
+    $(document).scroll(function(e) {
+      updateHeader();
     })
 
     function isBelowTitle() {
       if(window.scrollY > titleDisplacement) {
         return true;
       }
+      return false;
     }
+
+    function updateHeader() {
+      if(headerIsShown && !isBelowTitle()) {
+        $("header").removeClass("is-shown").addClass("is-hidden");
+        headerIsShown = false;
+      }
+      else if(!headerIsShown && isBelowTitle()) {
+        $("header").removeClass("is-hidden").addClass("is-shown");
+        headerIsShown = true;
+      }
+    }
+
   }
 })
